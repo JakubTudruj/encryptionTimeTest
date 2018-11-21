5369d7a4c16ac917471477d04cc2409aee36995b
Jakub Tudruj <jtudruj@hotmail.com>
2018-11-21 01:00:28 +0100

    added empty Encryptor class

---
+++ encryptionTimeTest/Utilities/Encryptor.swift
@@ -0,0 +1,22 @@
+//
+//  Encryptor.swift
+//  encryptionTimeTest
+//
+//  Created by Jakub Tudruj on 21/11/2018.
+//  Copyright © 2018 Jakub Tudruj. All rights reserved.
+//
+
+import CommonCrypto
+import Foundation
+
+class Encryptor {
+    
+    func encrypt(data: Data, using algorithm: SecKeyAlgorithm, with key: SecKey) {
+        
+    }
+    
+    func decrypt(data: Data, using algorithm: SecKeyAlgorithm, with key: SecKey) {
+        
+    }
+    
+}
